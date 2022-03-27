95092c372bc04323d072bc8cac39ca6aa8c5c322
Vany Liu <vany8462@gmail.com>
2022-03-27 18:39:22 +0800

    use local storage to keep weather ui as in last search when refresh page

--- scripts/app.js
+++ scripts/app.js
@@ -28,13 +28,6 @@ const updateUI = (data) => {
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
 
-    // let timeSrc = null;
-    // if(weather.IsDayTime){
-    //     timeSrc = 'img/day.svg';
-    // } else {
-    //     timeSrc = 'img/night.svg';
-    // }
-
     let timeSrc =  weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
 
     time.setAttribute('src', timeSrc);
@@ -65,4 +58,13 @@ cityForm.addEventListener('submit', e => {
     updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err));
+
+        //set local storage
+        localStorage.setItem('city',city);
 });
+
+if(localStorage.getItem('city')){
+    updateCity(localStorage.getItem('city'))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+};
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -17,8 +17,9 @@ const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
-    const response = await fetch(base + query); 
+    const response = await fetch(base + query);
     const data = await response.json();
+
     return data[0];
 
 };
